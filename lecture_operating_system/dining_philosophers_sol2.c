081915b5dbe2098c050d7fbc38ffa107fee15112
logicallaw <logicallawbio@gmail.com>
2025-06-03 13:01:57 +0900

    feat: add dining_philosophers_sol2.c

---
+++ lecture_operating_system/dining_philosophers_sol2.c
@@ -0,0 +1,81 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#define NUM 5
+
+/*
+Solution 2:
+Deadlock Prevention의 방법 중 하나인 No Hold and Wait으로 Deadlock condition
+예방.
+*/
+
+sem_t forks[NUM]; // forks. 공유자원임. 이 문제에선 Binary Semaphore를 나타냄.
+sem_t once;
+
+void pickup(int philosopher_num) { sem_wait(&forks[philosopher_num % NUM]); }
+
+void putdown(int philosopher_num) { sem_post(&forks[philosopher_num % NUM]); }
+
+void thinking(int philosopher_num) {
+  printf("philosopher %d is thinking\n", philosopher_num);
+  return;
+}
+
+void eating(int philosopher_num) {
+  printf("philosopher %d is eating\n", philosopher_num);
+  return;
+}
+
+void *philosopher(void *arg) {
+  int philosopher_num;
+  philosopher_num = (unsigned long int)arg;
+
+  while (1) {
+    sem_wait(&once);
+    pickup(philosopher_num);
+    printf("philosopher %d picks up the fork %d.\n", philosopher_num,
+           philosopher_num);
+    pickup(philosopher_num + 1);
+    printf("philosopher %d picks up the fork %d.\n", philosopher_num,
+           (philosopher_num + 1) % NUM);
+    sem_post(&once);
+
+    eating(philosopher_num);
+
+    putdown(philosopher_num);
+    printf("philosopher %d puts down the fork %d.\n", philosopher_num,
+           (philosopher_num + 1) % NUM);
+    putdown(philosopher_num + 1);
+    printf("philosopher %d puts down the fork %d.\n", philosopher_num,
+           philosopher_num);
+
+    thinking(philosopher_num);
+  }
+  return NULL;
+}
+
+int main() {
+  // 스레드 ID 배열 선언
+  pthread_t threads[NUM];
+
+  for (int i = 0; i < NUM; i++) {
+    // 세마포어 변수들을 초기화함.
+    // 두 번째 인자의 0은 스레드간 공유자원을 의미하며, 세 번째 인자의 1은
+    // 세마포어 초깃값을 나타낸다.
+    // 즉, 포크 1개를 사용할 수 있다는 의미다.
+    sem_init(&forks[i], 0, 1);
+  }
+
+  sem_init(&once, 0, 1);
+
+  for (unsigned long int i = 0; i < NUM; i++) {
+    pthread_create(&threads[i], NULL, philosopher, (void *)i);
+  }
+
+  for (int i = 0; i < NUM; i++) {
+    pthread_join(threads[i], NULL);
+  }
+
+  printf("NO DEADLOCK\n");
+  return 0;
+}
